3eb1fc9d786ce78081ed524bcdf05a167cb27911
imam-programmer <imamprogrammer09@gmail.com>
2026-07-12 22:21:22 +0600

    banner slider done & also banner done

--- src/components/layout/home/Banner.jsx
+++ src/components/layout/home/Banner.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 import bannerData from "../../../api/bannerdata.json";
 import Image from "../common/Image";
+import 'swiper/css';
+import 'swiper/css/pagination';
 const Banner = () => {
   console.log(bannerData);
   return (
     <section className="flex">
- 
-
-
-      {bannerData.map((item) => (
-        <Image className="w-full" src={item.image} alt="banner" />
-      ))}
-
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {bannerData.map((item) => (
+          <SwiperSlide>
+            <Image className="w-full" src={item.image} alt="banner" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
