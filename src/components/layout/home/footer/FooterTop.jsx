adcb751eb92c67a4556fffbe9e0df10f030d7e37
imam-programmer <imamprogrammer09@gmail.com>
2026-07-29 00:31:12 +0600

    footertop part 4 done

--- src/components/layout/home/footer/FooterTop.jsx
+++ src/components/layout/home/footer/FooterTop.jsx
@@ -3,12 +3,14 @@ import Image from '../../common/Image'
 import FooterTop1 from './FooterTop1'
 import FooterTop2 from './FooterTop2'
 import FooterTop3 from './FooterTop3'
+import FooterTop4 from './FooterTop4'
 const FooterTop = () => {
     return (
         <div className='container flex justify-between'>
             <FooterTop1/>
             <FooterTop2/>
             <FooterTop3/>
+            <FooterTop4/>
         </div>
     )
 }
