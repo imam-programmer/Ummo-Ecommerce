0bf9ae77499afc46ac93ab7f6ff5404352c9fb15
imam-programmer <imamprogrammer09@gmail.com>
2026-07-28 10:45:47 +0600

    spring part done by backend time use

---
+++ src/components/layout/home/SpringCollection.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import SpringImage from "../../../assets/images/SpringCollection.png"
+import Image from '../common/Image'
+import { Link } from 'react-router'
+import Countdown from 'react-countdown';
+const SpringCollection = () => {
+    const Completionist = () => <span>You are good to go!</span>;
+
+
+    const renderer = ({ days, hours, minutes, seconds }) => {
+
+        return (
+            <div className='flex gap-6.25 mt-29.75'>
+                <div>
+
+                    <h2 className='text-[30px] font-normal text-primary'>{days}</h2>
+                    <h3 className='text-[16px] font-bold text-gray uppercase'>DAYS</h3>
+                </div>
+
+                <h2 className='text-[30px] font-normal text-primary'>:</h2>
+                <div>
+
+                    <h2 className='text-[30px] font-normal text-primary'>{hours}</h2>
+                    <h3 className='text-[16px] font-bold text-gray uppercase'>HOURS</h3>
+                </div>
+
+                <h2 className='text-[30px] font-normal text-primary'>:</h2>
+                <div>
+
+                    <h2 className='text-[30px] font-normal text-primary'>{minutes}</h2>
+                    <h3 className='text-[16px] font-bold text-gray uppercase'>MINS</h3>
+                </div>
+
+                <h2 className='text-[30px] font-normal text-primary'>:</h2>
+                <div>
+
+                    <h2 className='text-[30px] font-normal text-primary'>{seconds}</h2>
+                    <h3 className='text-[16px] font-bold text-gray uppercase'>SEC</h3>
+                </div>
+
+
+            </div>
+        )
+
+    };
+
+
+
+    return (
+        <section className='bg-[#ebebeb] py-7'>
+            <div className="container">
+                <div className='flex justify-between items-end'>
+                    <div>
+                        <h3 className='text-sm font-medium leading-6 text-[#c32929]  after:content-[""] after:duration-300 after:w-10 after:h-0.5 after:bg-[#c32929] after:absolute after:top-2 relative after:-left-15 ml-15'>DEAL OF THE WEEK</h3>
+                        <h2 className='uppercase text-[70px] font-normal text-primary'><span className='font-bold'>Spring</span> Collection</h2>
+                        <Link className='uppercase text-primary text-sm leading-6 after:content-[""] after:w-0 after:duration-300 hover:after:w-12.5 after:h-0.75 after:bg-primary after:absolute after:bottom-0 relative after:left-0 font-normal'>
+                            SHOP NOW
+                        </Link>
+
+
+                        <Countdown
+                            date={"2026-12-31T14:22:56.397+00:00"}
+                            renderer={renderer}
+                        />
+                    </div>
+                    <div><Image src={SpringImage} alt="springimage" /></div>
+                </div>
+
+
+            </div>
+        </section>
+    )
+}
+
+export default SpringCollection
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/layout/home/Banner'
 import Category from '../components/layout/home/Category'
 import TrendyProducts from '../components/layout/home/TrendyProducts'
+import SpringCollection from '../components/layout/home/SpringCollection'
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
    <Banner/>
    <Category/>
    <TrendyProducts/>
+   <SpringCollection/>
    </>
   )
 }
