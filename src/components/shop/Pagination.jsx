a4417076059ba81fb49f71649bb1cde8e41a1e04
imam-programmer <imamprogrammer09@gmail.com>
2026-08-17 23:08:30 +0600

    shop banner list set by api and head tag set dynamically

--- src/components/shop/Pagination.jsx
+++ src/components/shop/Pagination.jsx
@@ -13,8 +13,8 @@ setfirst(arr)
 }, [])
 
   return (
-    <div>{first.map((item)=>(
-        <button onClick={()=>CurrPage(item)} className='text-[18px]   focus:bg-green-700 focus:text-white cursor-pointer  ml-2  w-7 border border-green-400 rounded-sm hover:bg-green-700'>{item}</button>
+    <div>{first.map((item,idx)=>(
+        <button key={idx} onClick={()=>CurrPage(item)} className='text-[18px]   focus:bg-green-700 focus:text-white cursor-pointer  ml-2  w-7 border border-green-400 rounded-sm hover:bg-green-700'>{item}</button>
     ))
     }
     </div>
--- src/components/shop/ShopBanner.jsx
+++ src/components/shop/ShopBanner.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "../layout/common/Image"
 import shopBg from "../../assets/images/shopBanner.png"
+import ShopBannerData from "../../api/ShopBannerData.json"
 const ShopBanner = () => {
+    const [first, setfirst] = useState(null)
     return (
         <div className='px-15 relative'>
             <Image className="w-full" src={shopBg} alt="shopbg" />
             <div className='absolute top-2/4 left-2/4 -translate-2/4 text-center '>
-                <h2 className='font-bold text-[60px] text-primary uppercase'>shoes</h2>
+                <h2 className='font-bold text-[60px] text-primary uppercase'>{first==1?"#STAYHOME":first==2?"NEW IN":first==3?"JACKETS":first==4?"HOODIES":first==5?"MEN":first==6?"WOMEN":first==7?"TROUSERS":first==8?"ACCESSORIES":"SHOES"}</h2>
                 <ul className='flex justify-center gap-7.5'>
-                    <li className='font-medium text-base text-primary'>#STAYHOME</li>
-                    <li className='font-medium text-base text-primary whitespace-nowrap'> NEW IN</li>
-                    <li className='font-medium text-base text-primary'> JACKETS</li>
-                    <li className='font-medium text-base text-primary'>HOODIES</li>
-                    <li className='font-medium text-base text-primary'>MEN</li>
-                    <li className='font-medium text-base text-primary'>WOMEN</li>
-                    <li className='font-medium text-base text-primary'>TROUSERS</li>
-                    <li className='font-medium text-base text-primary'>ACCESSORIES</li>
-                    <li className='font-medium text-base text-primary'>SHOES</li>
+                    {ShopBannerData.map((item)=>(
+                    <li key={item.id} onClick={()=>setfirst(item.id)} className='font-medium text-base text-primary whitespace-nowrap cursor-pointer'>{item.name}</li>
+                    ))}
+                  
                 </ul>
             </div>
         </div>
