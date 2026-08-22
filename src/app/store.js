41db84b1245fe82505fadf604516d66491d927fb
imam-programmer <imamprogrammer09@gmail.com>
2026-08-22 07:08:23 +0600

    Redux setup done for data centralize

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterSlice  from '../slices/productSlice'
+
+export const store = configureStore({
+  reducer: {
+    counter:counterSlice
+  },
+})
