52e1b4f0fa6b361781cafb3c0a174e99d7890d9c
imam-programmer <imamprogrammer09@gmail.com>
2026-09-14 22:17:10 +0600

    starting page debugging solve

--- src/components/layout/home/Starting.jsx
+++ src/components/layout/home/Starting.jsx
@@ -8,13 +8,13 @@ const Starting = () => {
         <section className='md:mt-25 mt-3.75'>
             <div className="container">
                 <div className='flex justify-between gap-y-3.75 flex-col md:flex-row md:gap-2 '>
-                    <div className='md:w-172.5 h-77.5 md:h-99.5  bg-[#D6001C] flex justify-between md:pl-12.5 pl-5.75 md:pr-2.5 items-end'>
+                    <div className='md:w-172.5 h-77.5 md:h-99.5  bg-[#D6001C] flex justify-between lg:pl-12.5 pl-5.75 md:pr-2.5 items-end'>
 
                         <div className='mb-12.75'>
 
-                            <h3 className='text-[18px] whitespace-nowrap font-medium  text-white'>STARTİNG AT $39</h3>
-                            <h2 className='md:text-[26px] whitespace-nowrap text-[22px]  font-medium  shrink-0 text-white mb-3.75 mt-2'>Women’s T-Shirts</h2>
-                            <Link className='uppercase text-white text-sm leading-6 after:content-[""] after:w-0 after:duration-300 hover:after:w-12.5 after:h-0.5 after:bg-white after:absolute after:bottom-0 relative after:left-0 font-medium'>
+                            <h3 className='lg:text-[18px] whitespace-nowrap font-medium  text-white'>STARTİNG AT $39</h3>
+                            <h2 className='lg:text-[26px] whitespace-nowrap text-[22px]  font-medium  shrink-0 text-white lg:mb-3.75 lg:mt-2'>Women’s T-Shirts</h2>
+                            <Link className='uppercase text-white text-[12px] lg:text-sm leading-6 after:content-[""] after:w-0 after:duration-300 hover:after:w-12.5 after:h-0.5 after:bg-white after:absolute after:bottom-0 relative after:left-0 font-medium'>
                                 SHOP NOW
                             </Link>
                         </div>
@@ -28,10 +28,10 @@ const Starting = () => {
 
                     <div className='md:w-172.5 h-77.5 md:h-99.5 relative'>
                         <Image className="w-full h-full" src={startingimage2} alt="startingimage2" />
-                        <div className='absolute bottom-12.75 md:left-12.5 left-5.75'>
-                            <h3 className='text-lg font-medium text-primary '>STARTİNG AT $39</h3>
-                            <h2 className='text-[22px] md:text-[26px] font-medium text-primary  mb-3.75 mt-2'>Men’s Sportswear</h2>
-                            <Link className='uppercase text-primary text-sm leading-6 after:content-[""] after:w-0 after:duration-300 hover:after:w-12.5 after:h-0.5 after:bg-primary after:absolute after:bottom-0 relative after:left-0 font-medium'>
+                        <div className='absolute bottom-12.75 lg:left-12.5 left-5.75'>
+                            <h3 className='lg:text-lg font-medium text-primary '>STARTİNG AT $39</h3>
+                            <h2 className='text-[22px] lg:text-[26px] font-medium text-primary  lg:mb-3.75 lg:mt-2 '>Men’s Sportswear</h2>
+                            <Link className='uppercase text-primary text-[12px] lg:text-sm leading-6 after:content-[""] after:w-0 after:duration-300 hover:after:w-12.5 after:h-0.5 after:bg-primary after:absolute after:bottom-0 relative after:left-0 font-medium'>
                                 SHOP NOW
                             </Link>
 
